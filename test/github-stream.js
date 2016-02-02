673571151a091a50bfbeaa10715375b20ef97c84
Ludo237 <ludovico1990@hotmail.it>
2016-02-02 17:10:39 +0100

    Added a sort of test, need to improve

---
+++ test/github-stream.js
@@ -0,0 +1,18 @@
+import Vue from "vue"
+import GithubStream from "src/github-stream.vue";
+
+// Component test
+describe("github-stream.vue", () => {
+  // Asserting that the component has been created
+  it("should have a created hook", () => {
+    expect(typeof GithubStream.created).toBe("function");
+  });
+
+  // Asserting that the component has the correct default data
+  it("should set correct default api URLs", () => {
+    expect(typeof GithubStream.data).toBe("function");
+    var defaultData = GithubStream.data();
+    expect(defaultData.api.base).toBe("https://api.github.com/users/");
+    expect(defaultData.api.activities).toBe("/events/public");
+  });
+});
